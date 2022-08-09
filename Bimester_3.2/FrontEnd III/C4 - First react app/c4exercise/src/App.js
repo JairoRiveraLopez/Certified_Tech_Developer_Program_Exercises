5a06c56181d265c7852a6c63be8c6d9d8a52ea44
unknown <jairorive96@gmail.com>
2022-08-08 21:21:56 -0500

    Adding c4 exercise react class

---
+++ Bimester_3.2/FrontEnd III/C4 - First react app/c4exercise/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>C4 Exercise - DH G6</h1>
+      <img src="https://github.com/Ivanszs/dh-frontend3-clases/blob/master/clase-4/satisfied.png?raw=true" alt= "happyMan"></img>
+    </div>
+  );
+}
+
+export default App;
