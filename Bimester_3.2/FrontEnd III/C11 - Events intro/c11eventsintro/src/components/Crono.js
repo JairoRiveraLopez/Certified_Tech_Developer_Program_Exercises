73c2041484b770c0320e6b9682f94b066bd5733d
unknown <jairorive96@gmail.com>
2022-08-31 21:58:23 -0500

    updated crono exercise

--- Bimester_3.2/FrontEnd III/C11 - Events intro/c11eventsintro/src/components/Crono.js
+++ Bimester_3.2/FrontEnd III/C11 - Events intro/c11eventsintro/src/components/Crono.js
@@ -1,29 +1,35 @@
 import { useEffect, useState } from "react";
 
-const Crono = () => {
-    let [number, setNumber] = useState(0)
-    let [status, setStatus] = useState("initialized")
+const Stopwatch = () => {
+    const [time, setTime] = useState(0);
+    const [status, setStatus] = useState(false);
 
-    useEffect( () => {
+    useEffect(() => {
         let interval;
-        if(status === "started"){
-            interval = setInterval(() => {
-            setNumber(number + 1)
-            }, 1000)
-        }else{
-            clearInterval(interval)
-            setNumber(0)
-        }
-    }, [status, number])
-
+    if (status === "started") {
+        interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+        }, 10);
+    } else if ("paused") {
+        clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+    }, [status]);
+    
     return (
-        <>
-        <h2>Crono App</h2>
-        <p>{number}</p>
-        <button onClick={() => setStatus("started")} >Start</button>
-        <button onClick={() => setStatus("reset")} >Reset</button>
-        </>
-    )
-}
+    <div className="stopwatch">
+        <div className="numbers">
+            <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+            <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+            <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div className="buttons">
+            <button onClick={() => setStatus("started")}>Start</button>
+            <button onClick={() => setStatus("paused")}>Stop</button>
+            <button onClick={() => setTime(0)}>Reset</button>
+        </div> 
+    </div>
+    );
+};
 
-export default Crono;
+export default Stopwatch;
